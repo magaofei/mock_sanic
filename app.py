381c9404cd90f5823ee494c9c33ddee7f4158f5a
magaofei <magaofei@100tal.com>
2018-04-11 10:07:54 +0800

    update host and port

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ app = Sanic()
 # seconds
 WAIT_TIME = 0.2
 
+HOST = '0.0.0.0'
+PORT = 8010
+
 @app.route('/')
 async def test(request):
     await sleep(WAIT_TIME)
     return json({'hello': 'world'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8010)
+    app.run(host=HOST, port=PORT)
